834b74741ab3ac0be6cbd92f4145113da8259c20
Ewout Merckx <ewout.merckx@kahosl.be>
2014-04-10 16:49:06 +0200

    Assembly information changed
    The assembly information has been changed: name, title and product

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OS")]
+[assembly: AssemblyTitle("Filedrops OS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xplore+")]
-[assembly: AssemblyProduct("OS")]
+[assembly: AssemblyProduct("Filedrops OS")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
