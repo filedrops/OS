379019df4c593c20c0e1c6656ca42843044e8311
Ewout Merckx <ewout.merckx@kahosl.be>
2014-04-10 11:37:08 +0200

    Company name added
    The company name has been added to the assembly information

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("xplore+")]
 [assembly: AssemblyProduct("OS")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
